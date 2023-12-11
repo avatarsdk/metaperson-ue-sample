73954656d3ce1f11ca1e60e386b9390ed6ce811c
nsdfxela nsdfxela <alexander.fedosin@itseez3d.com>
2023-12-11 14:27:48 +0300

    Moved-all-stuff-inside-plugin

---
+++ Plugins/AvatarSDKMetaperson2/Source/AvatarSDKMetaperson2/Source/AvatarSDKMetaperson2/Private/Metaperson2GameMode.cpp
@@ -0,0 +1,26 @@
+/* Copyright (C) Itseez3D, Inc. - All Rights Reserved
+ * You may not use this file except in compliance with an authorized license
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * UNLESS REQUIRED BY APPLICABLE LAW OR AGREED BY ITSEEZ3D, INC. IN WRITING, SOFTWARE DISTRIBUTED UNDER THE LICENSE IS DISTRIBUTED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED
+ * See the License for the specific language governing permissions and limitations under the License.
+ * Written by Itseez3D, Inc. <support@itseez3D.com>, May 2023
+ */
+
+#include "Metaperson2GameMode.h"
+#include "Metaperson2Character.h"
+#include "UObject/ConstructorHelpers.h"
+#include "GameFramework/HUD.h"
+
+AMetaperson2GameMode::AMetaperson2GameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/AvatarSDKMetaperson2/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<AHUD> HUDBpClass(TEXT("/AvatarSDKMetaperson2/ThirdPerson/Blueprints/BP_HUD")); 
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+		HUDClass = HUDBpClass.Class;
+	}
+}
---
+++ Plugins/AvatarSDKMetaperson2/Source/AvatarSDKMetaperson2/Source/AvatarSDKMetaperson2/Public/Metaperson2GameMode.h
@@ -0,0 +1,27 @@
+/* Copyright (C) Itseez3D, Inc. - All Rights Reserved
+ * You may not use this file except in compliance with an authorized license
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * UNLESS REQUIRED BY APPLICABLE LAW OR AGREED BY ITSEEZ3D, INC. IN WRITING, SOFTWARE DISTRIBUTED UNDER THE LICENSE IS DISTRIBUTED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED
+ * See the License for the specific language governing permissions and limitations under the License.
+ * Written by Itseez3D, Inc. <support@itseez3D.com>, May 2023
+ */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Metaperson2GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMetaperson2GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMetaperson2GameMode();
+};
+
+
+
