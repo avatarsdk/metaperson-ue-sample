c39ab4b15e4dd592a86332f4128015e81096a2bc
nsdfxela nsdfxela <alexander.fedosin@itseez3d.com>
2024-10-01 17:44:23 +0300

    added-module

---
+++ Plugins/AvatarSDKMetaperson2/Source/AvatarSDKMetaperson2Editor/Private/AvatarSDKMetaperson2Editor.cpp
@@ -0,0 +1,30 @@
+/* Copyright (C) Itseez3D, Inc. - All Rights Reserved
+ * You may not use this file except in compliance with an authorized license
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * UNLESS REQUIRED BY APPLICABLE LAW OR AGREED BY ITSEEZ3D, INC. IN WRITING, SOFTWARE DISTRIBUTED UNDER THE LICENSE IS DISTRIBUTED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED
+ * See the License for the specific language governing permissions and limitations under the License.
+ * Written by Itseez3D, Inc. <support@itseez3D.com>, May 2023
+ */
+
+#include "AvatarSDKMetaperson2Editor.h"
+#include "Developer/Settings/Public/ISettingsModule.h"
+
+
+#define LOCTEXT_NAMESPACE "FAvatarSDKMetaperson2EditorModule"
+
+void FAvatarSDKMetaperson2EditorModule::StartupModule()
+{
+	 
+}
+
+void FAvatarSDKMetaperson2EditorModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FAvatarSDKMetaperson2EditorModule, AvatarSDKMetaperson2Editor)
---
+++ Plugins/AvatarSDKMetaperson2/Source/AvatarSDKMetaperson2Editor/Public/AvatarSDKMetaperson2Editor.h
@@ -0,0 +1,26 @@
+/* Copyright (C) Itseez3D, Inc. - All Rights Reserved
+ * You may not use this file except in compliance with an authorized license
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * UNLESS REQUIRED BY APPLICABLE LAW OR AGREED BY ITSEEZ3D, INC. IN WRITING, SOFTWARE DISTRIBUTED UNDER THE LICENSE IS DISTRIBUTED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED
+ * See the License for the specific language governing permissions and limitations under the License.
+ * Written by Itseez3D, Inc. <support@itseez3D.com>, May 2023
+ */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+//DECLARE_LOG_CATEGORY_CLASS(LogMetaperson2, Log, All);
+
+
+class FAvatarSDKMetaperson2EditorModule : public IModuleInterface
+{
+public:
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
